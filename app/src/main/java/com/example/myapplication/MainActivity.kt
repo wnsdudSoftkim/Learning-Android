7302843f00811d698213b178c2a51722c10d19d7
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2020-07-06 20:05:00 +0900

    setOnclickListner 등등 메소드 공부

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //  val btnlogin : Button = findViewById(R.id.btn_login)
+//        btn_login.setOnClickListener {
+//            btn_login.setText("준영")
+//        }
+
     }
+
 }
